eca99eb0dd29a1261b55762dfdd3a1bdf7015726
helen <helen@vm-1.helen.koding.kd.io>
2014-09-21 05:57:50 +0000

    unit tests

---
+++ test/unit/classy.initScope.spec.coffee
@@ -0,0 +1,46 @@
+﻿# Test Data
+
+app = angular.module 'classyInitScopeTest', [
+    'classy'
+    'classy-initScope'
+]
+        
+app.classy.controller
+    name: 'InitController'
+   
+    inject: ['$scope']
+   
+    initScope: ->
+        a: 'a'
+        b: 'b'
+        c: 'c'
+        objectA: 
+            item1: 1
+            item2: 2 
+        
+# Tests
+
+describe 'Classy initScope (classy-initScope.coffee)', ->
+    beforeEach module 'classyInitScopeTest'
+    
+    initController = null
+    scope = null
+    
+    beforeEach ->
+        inject ($controller, $rootScope) ->
+            scope = $rootScope.$new()
+            initController = $controller 'InitController', 
+                $scope: scope
+            return
+        return
+    
+    it 'should initialize scope values', ->
+        expect(scope.a).toBe 'a'
+        expect(scope.b).toBe 'b'
+        expect(scope.c).toBe 'c'
+        expect(scope.objectA).toEqual
+            item1: 1
+            item2: 2
+        
+        return
+         
---
+++ test/unit/classy.initScope.spec.js
@@ -0,0 +1,46 @@
+﻿(function() {
+  var app;
+
+  app = angular.module('classyInitScopeTest', ['classy', 'classy-initScope']);
+
+  app.classy.controller({
+    name: 'InitController',
+    inject: ['$scope'],
+    initScope: function() {
+      return {
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        objectA: {
+          item1: 1,
+          item2: 2
+        }
+      };
+    }
+  });
+
+  describe('Classy initScope (classy-initScope.coffee)', function() {
+    var initController, scope;
+    beforeEach(module('classyInitScopeTest'));
+    initController = null;
+    scope = null;
+    beforeEach(function() {
+      inject(function($controller, $rootScope) {
+        scope = $rootScope.$new();
+        initController = $controller('InitController', {
+          $scope: scope
+        });
+      });
+    });
+    return it('should initialize scope values', function() {
+      expect(scope.a).toBe('a');
+      expect(scope.b).toBe('b');
+      expect(scope.c).toBe('c');
+      expect(scope.objectA).toEqual({
+        item1: 1,
+        item2: 2
+      });
+    });
+  });
+
+}).call(this);
